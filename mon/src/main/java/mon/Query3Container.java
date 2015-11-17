9c0b8a9f9ee875e7d0441682fbc6f506d871c2c8
Pau Mascarell <pau.mascarell@est.fib.upc.edu>
2015-11-17 23:32:46 +0100

    query3

---
+++ mon/src/main/java/mon/Query3Container.java
@@ -0,0 +1,9 @@
+package mon;
+
+public class Query3Container {
+		 public Double revenue;
+		 public String orderKey;
+		 public String orderDate;
+		 public String orderShip;
+
+	}
---
+++ mon/src/main/java/mon/Query3Result.java
@@ -0,0 +1,25 @@
+package mon;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Query3Result {
+	public ArrayList<Query3Container> list;
+	
+	public void push(Double d, String orderKey, String orderDate, String orderShip){
+		Query3Container q3 = new Query3Container();
+		q3.orderDate = orderDate;
+		q3.orderKey = orderKey;
+		q3.orderShip = orderShip;
+		q3.revenue = d;
+		list.add(q3);
+	}
+	 public void sort(){
+	        Collections.sort(list, new Comparator() {
+				public int compare(Object o1, Object o2) {
+					return (((Query3Container)o1).revenue > ((Query3Container)o2).revenue) ? 1 : -1;
+				}
+	        });
+		 }
+}
